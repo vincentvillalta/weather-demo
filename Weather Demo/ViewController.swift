7b1693f02a1043df3aa78bc7747bcbecade1f3c3
Vincent Villalta <vincentvillalta@me.com>
2020-02-13 16:01:30 -0500

    Initial Commit

---
+++ Weather Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather Demo
+//
+//  Created by Vincent Villalta on 2/13/20.
+//  Copyright © 2020 vincentvillalta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
